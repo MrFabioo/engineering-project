10a15f70a899575927521050bfe5ba23f2c41255
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-18 04:25:50 +0200

    Firebase Setup

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,3 @@
-import { css } from 'styled-components';
-
 export const theme = {
   colors: {
     accent: '#ED2553',
@@ -17,17 +15,3 @@ export const theme = {
     maxWidth: '460px',
   },
 };
-
-export const materialShadow = ({ level }) => css`
-  @if $level == 1 {
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  } @else if $level == 2 {
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  } @else if $level == 3 {
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  } @else if $level == 4 {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  } @else if $level == 5 {
-    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-  }
-`;
